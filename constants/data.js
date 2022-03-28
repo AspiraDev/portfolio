7fc4745913c5dccae7ab4841f0f1a79e073b9021
AspiraDev <protocoloone@gmail.com>
2022-03-28 13:46:17 -0300

    primeiro

---
+++ constants/data.js
@@ -0,0 +1,64 @@
+const userData = {
+  githubUsername: "AspiraDev",
+  name: "Rogério Oliveira",
+  designation: "Desenvolvedor Python",
+  avatarUrl: "/avatar.png",
+  email: "protocoloone@gmail.com",
+  phone: "+55 (73) 98897-1154",
+  address: "Franca, SP",
+  projects: [
+    {
+      title: "API REST - LGPD",
+      link: "https://api-lgpd.protocoloone.repl.co",
+      imgUrl: "/tmk.jpg",
+    },
+    {
+      title: "Portfólio",
+      link: "https://AspiraDev.github.io/portfolio/",
+      imgUrl: "/portfolio.png",
+    },
+    {
+      title: "Site do meu Mochilão",
+      link: "https://primeiro.mochilao.repl.co",
+      imgUrl: "/mochilao.png",
+    },
+  ],
+  about: {
+    title:
+      "Sou desenvolvedor Python autodidata em busca de uma oportunidade na área.",
+    description: [
+      `Tenho 22 anos e sou estudante de direito, mas apaixonado pela área de tecnologia e desenvolvimento de aplicações.`,
+      `Quando me formar, planejo trabalhar na área de Direito Digital. `,
+      `Atualmente estou em busca de uma oportunidade na área de programação, em específico Python.`,
+    ],
+   
+  },
+  experience: [
+  
+    {
+      title: "Suporte Técnico",
+      company: "Tel Telemática",
+      year: "2021",
+      desc: "Prestei suporte técnico na área de redes, com o foco em telecomunicações; ",
+    },
+    {
+      title: "Assistinte Administrátivo",
+      company: "TD TRANSPORTES",
+      year: "2020",
+      desc: "Fui responsável pela administração do escritório, emissão de notas fiscais, admissão e demissão de funcionários, calculo de férias e banco de horas, elaboração de relatório de gastos, dentre outros.",
+    },
+    {
+      title: "Estagiário",
+      company: "Prefeitura de Itabuna",
+      year: "2016",
+      desc: "Estagiei na Secretaria Municipal de Saúde com a função de operacionalizar os Sistemas de Saúde da Atenção Básica e-SUS PEC - Prontuário Eletrônico do Cidadão, CDS - Coleta de Dados Simplificado e CAD-WEB (Cartão Nacional e Municipal SUS).",
+    },
+  ],
+  socialLinks: {
+    linkedin: "https://www.linkedin.com/in/rogério-oliveira-9bb48b230/",
+    github: "https://github.com/AspiraDev",
+   
+  },
+};
+
+export default userData;
